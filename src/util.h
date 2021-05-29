dc6c97ccac9ab22edaa7bb36246e4504f46f6d20
masrul <mail2masrul@gmail.com>
2021-05-28 19:57:10 -0500

    remove std:: from util.h

--- src/util.h
+++ src/util.h
@@ -15,7 +15,7 @@ struct singleAtom{
 }; 
 
 struct MolTracker{
-    std::string resname; 
+    string resname; 
     int nAtoms ;
     int sIDx;  // starting index of molecule  
     int eIDx;  // end index of molecule 
@@ -28,9 +28,9 @@ struct COM{
     float max_dist; 
 }; 
 
-extern int str2int(std::string str);
-extern float str2float(std::string str); 
-extern singleAtom line2coord(std::string line); 
+extern int str2int(string str);
+extern float str2float(string str); 
+extern singleAtom line2coord(string line); 
 extern void applyPBC(float &dx, float &dy, float &dz,float* box); 
     
 
